1b236a23997c851ed2835887893bf8baa16b9dc8
saifulemon <463saiful0@gmail.com>
2023-03-02 21:57:49 +0600

    finished the tic tac toe app

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Square = (props) => {
     return (
         <div>
-            <button className="btn" onClick={props.onClick}>{props.value}</button>
+            <button className={props.value ? 'btn disabled' : 'btn'} onClick={props.onClick}>{props.value}</button>
         </div>
     );
 };
